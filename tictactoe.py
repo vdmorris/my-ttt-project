8d3d65612e78a267000d8259a5257e56136edaba
vdmorris <104547323+vdmorris@users.noreply.github.com>
2022-05-03 00:45:54 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,23 +1,34 @@
-def main():
-        player = swap_player("")
-        board = create_board()
-        show_board(board)
-        print("Good game. Thanks for playing!") 
+'''
+Assignment: Tic-Tac-Toe
+Author: Vanessa Morris-Cartagena
+'''
 
-def create_board():
+def main():
+        player = swap("")
+        board = create()
+        while not (check_win(board) or player_tie(board)):
+            draw(board)
+            input_move(player, board)
+            player = swap(player)
+        draw(board)
+        print("Thanks for playing! Play again!") 
+
+def create():
     board = []
+    for cell in range(9):
+        board.append(cell + 1)
     return board
 
-def show_board(board):
+def draw(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-<>-<>-')
+    print('<>-<>')
     print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-<>-<>-')
+    print('<>-<>')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
 
-def player_draw(board):
+def player_tie(board):
     for cell in range(9):
         if board[cell] != "x" and board[cell] != "o":
             return False
@@ -28,10 +39,17 @@ def input_move(player, board):
     board[cell - 1] = player
 
 def check_win(board):
-     return ""
-    
+    return (board[0] == board[1] == board[2] or
+    board[3] == board[4] == board[5] or
+    board[6] == board[7] == board[8] or
+    board[0] == board[3] == board[6] or
+    board[1] == board[4] == board[7] or
+    board[2] == board[5] == board[8] or
+    board[0] == board[4] == board[8] or
+    board[2] == board[4] == board[6])
 
-def swap_player(current):
+
+def swap(current):
     if current == "" or current == "o":
         return "x"
     elif current == "x":
